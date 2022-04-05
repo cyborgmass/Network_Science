239125f59d05d5c8da6cab3201bcf2dd2e446db9
Masoud Ahangary <cyborgmass@gmail.com>
2022-04-05 14:36:08 +0430

    Second

--- NetR/ui.R
+++ NetR/ui.R
@@ -5,7 +5,7 @@ shinyUI(
   
   fixedPage(
     
-    tags$head(tags$link(rel = "stylesheet",type = "text/css",href = "styless.css")),
+    tags$head(tags$link(rel = "stylesheet",type = "text/css",href = "styles.css")),
     tags$head(tags$link(rel="stylesheet" ,href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")),
     
     
